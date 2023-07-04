cf89eae15f6fd627d7e2c210047f201b54d9df1c
Siddharth Gupta <sidd271299@gmail.com>
2023-07-04 13:19:05 +0530

    push

---
+++ demoo/src/components/Counter.js
@@ -0,0 +1,32 @@
+
+import { useState } from "react"
+
+
+
+
+function Counter(){
+    const[count,setCount]=useState(0)
+
+    function increment(){
+        setCount(count+1);
+    }
+function decrement(){
+    if(count==0){
+    setcount(count);
+    }
+    else{
+        setCount(count-1);
+    }
+
+}
+    return(
+        <div>
+            <h2>The count value is </h2>
+
+            <button onclick={increment}>increment</button>
+            <button onclick={decrement}>decrement</button>
+
+        </div>
+    )
+}
+export default Counter
